f13362a790f5e8ec46b8002690875aeddb5122ba
manshikumari67 <manshikumari345@gmail.com>
2026-05-01 17:13:46 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,11 @@ connectDB();
 cloudinaryConnect(); // 🔥 IMPORTANT
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL, 
+    credentials: true,
+  }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
